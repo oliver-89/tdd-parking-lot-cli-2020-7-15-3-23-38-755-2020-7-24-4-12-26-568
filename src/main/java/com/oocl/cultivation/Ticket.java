416e7314f3df4e198dd2242f2d44a02f65d8d951
oliver-89 <wjr980729@163.com>
2020-07-24 16:49:30 +0800

    [parking lot] [oliver] feature:caret Ticket class

---
+++ src/main/java/com/oocl/cultivation/Ticket.java
@@ -0,0 +1,17 @@
+package com.oocl.cultivation;
+
+import java.util.Map;
+
+public class Ticket {
+    private String numberPlate;
+
+    public Ticket(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+
+
+}
