41ca90e21f2ae90f1ef4268914d952bba811cd9d
oliver-89 <wjr980729@163.com>
2020-07-24 18:40:26 +0800

    [parking lot] [oliver] feature:add a fetching method

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,8 +1,14 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+
     public Ticket parking(Car car){
         Ticket ticket = new Ticket(car.getNumberPlate());
         return  ticket;
+    }
+
+    public Car fetching(Ticket ticket){
+
+
     }
 }
