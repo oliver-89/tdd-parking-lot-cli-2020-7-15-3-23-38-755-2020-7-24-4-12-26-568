134cc4ff0a4fe19abf47c0d8fe7b79ce9b214e8f
oliver-89 <wjr980729@163.com>
2020-07-24 16:48:20 +0800

    [parking lot] [oliver] feature:caret car class

---
+++ src/main/java/com/oocl/cultivation/Car.java
@@ -0,0 +1,13 @@
+package com.oocl.cultivation;
+
+public class Car {
+    private String numberPlate;
+
+    public Car(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+}
