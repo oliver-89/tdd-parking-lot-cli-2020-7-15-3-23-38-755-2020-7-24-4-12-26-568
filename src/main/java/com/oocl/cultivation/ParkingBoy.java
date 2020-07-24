5215fada29ce35c3a8ee7dd7a84b2e7fdc071bf5
oliver-89 <wjr980729@163.com>
2020-07-24 16:48:52 +0800

    [parking lot] [oliver] feature:caret ParkingBoy class

---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,8 @@
+package com.oocl.cultivation;
+
+public class ParkingBoy {
+    public Ticket parking(Car car){
+        Ticket ticket = new Ticket(car.getNumberPlate());
+        return  ticket;
+    }
+}
