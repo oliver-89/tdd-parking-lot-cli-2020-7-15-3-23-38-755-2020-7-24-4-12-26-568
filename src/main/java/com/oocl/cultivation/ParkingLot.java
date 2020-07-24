86b57f6c1bbbb5f9cdc876952af080f2553408b8
oliver-89 <wjr980729@163.com>
2020-07-24 16:49:07 +0800

    [parking lot] [oliver] feature:caret ParkingLot class

---
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -0,0 +1,16 @@
+package com.oocl.cultivation;
+
+import java.util.Map;
+
+public class ParkingLot {
+    private int totalNumber;
+
+    public ParkingLot() {
+        this.totalNumber = 0;
+    }
+
+    public int getTotalNumber() {
+        return totalNumber;
+    }
+
+}
