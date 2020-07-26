4b478cea91aa7afae0188df87c0abc5f089143f0
oliver-89 <wjr980729@163.com>
2020-07-26 16:16:36 +0800

    [parking lot] [oliver] feature: implement test  should_return_null_when_smart_parking_boy_fetching_given_1_car_1_smart_parking_boy_full_parking_lot

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -7,20 +7,31 @@ public class SmartParkingBoy extends ParkingBoy{
 
 
     public Ticket parking(Car car){
-        ParkingLot parkingLot = findLessParkingLot(getParkingLots());
-        Ticket ticket = new Ticket(car.getNumberPlate());
-        parkingLot.getCars().add(car);
-        parkingLot.setCapacity(parkingLot.getCapacity()+1);
-        return  ticket;
+        if(getTotalNumOfCars()<20){
+            ParkingLot parkingLot = findLessParkingLot(getParkingLots());
+            Ticket ticket = new Ticket(car.getNumberPlate());
+            parkingLot.getCars().add(car);
+            parkingLot.setCapacity(parkingLot.getCapacity()+1);
+            return  ticket;
 
+        }
+        return null;
 
+    }
+
+    public int getTotalNumOfCars(){
+        int num =0 ;
+        for(ParkingLot parkingLot:getParkingLots()){
+            num += parkingLot.getCapacity();
+        }
+        return num;
     }
 
     public ParkingLot findLessParkingLot(ArrayList<ParkingLot> parkingLots){
         ParkingLot parkingLot = null;
         int capacity = 10;
         for(ParkingLot thisParkingLot:parkingLots){
-            if(capacity>thisParkingLot.getCapacity()){
+            if(capacity>=thisParkingLot.getCapacity()){
                 parkingLot = thisParkingLot;
                 capacity = thisParkingLot.getCapacity();
             }
